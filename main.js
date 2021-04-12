d564881bfdf3ca1669309843f07cdac91c532813
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-04-12 09:58:49 +0300

    updating answer from the daily challenge

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//Remove Banana from the array.
+
 fruits.shift()
+
+// Sort the array in alphabetical order.
+
 fruits.sort()
+
+// Add “Kiwi” to the end of the array.
+
 fruits.push("Kiwi")
+
+// Remove “Apples” from the array. Don’t use the same method as in part 1.
+
 fruits.splice(0, 1)
+
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+
 fruits.reverse();
+
+// At the end you should see this outcome:
+
 
 console.log(fruits)
 
 
 let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-console.log(moreFruits[1][1]);
+console.log(moreFruits[1][1][0]);
+
+
